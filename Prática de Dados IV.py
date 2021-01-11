0f833d74d556c38d43cd48b4b03bc5d8a1fab14a
Wilson77Calixto <65840622+Wilson77Calixto@users.noreply.github.com>
2021-01-11 18:07:56 -0300

    Estudo de análise de dados csv

---
+++ Prática de Dados IV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ESTUDO DE DATASET CSV
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv')
+
+
+# In[ ]:
+
+
+
+
