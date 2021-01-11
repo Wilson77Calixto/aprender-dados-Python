81437b62a18582ac2c30190ee0b85ff629221961
Wilson77Calixto <65840622+Wilson77Calixto@users.noreply.github.com>
2021-01-11 11:31:13 -0300

    Criação de tabela com número inteiros

---
+++ Prática de dados I.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+s = pd.Series([1, 2, 3, 4, 5])
+
+
+# In[4]:
+
+
+s
+
+
+# In[6]:
+
+
+s.values
+
+
+# In[8]:
+
+
+s[3]
+
+
+# In[ ]:
+
+
+
+
